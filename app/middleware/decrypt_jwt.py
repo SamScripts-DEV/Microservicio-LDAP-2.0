fddec2c72359833ad990f31a47c3266d3a2a1790
No fui yo, fue el codigo <robsamuel.cuti@epn.edu.ec>
2025-11-07 10:41:13 -0500

    updated(logs)

--- app/middleware/decrypt_jwt.py
+++ app/middleware/decrypt_jwt.py
@@ -9,12 +9,14 @@ async def decrypt_request(request: Request):
 
     try:
         data = json.loads(body.decode("utf-8"))
+        print("Payload recibido:", data)
         if "token" not in data:
             raise HTTPException(status_code=422, detail="Falta campo 'token'")
         decrypted = jwt_service.decrypt_payload(data["token"])
-        # elimino iat/exp si quieres
+        print("Payload desencriptado:", decrypted)
         for k in ["iat","exp"]:
             decrypted.pop(k, None)
         return decrypted
     except Exception as e:
+        print("Error desencriptando:", e)
         raise HTTPException(status_code=422, detail=f"Error desencriptando payload: {str(e)}")
